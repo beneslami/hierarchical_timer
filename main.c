7a18c779f6da6805f0cf1f86bca08081b1945ec8
Benyamin <beneslami72@gmail.com>
2020-07-10 21:30:22 +0430

    add new special case

--- main.c
+++ main.c
@@ -15,11 +15,10 @@ void wrapper_print(void *arg, int arg_size){
 int
 main(int argc, char **argv){
   timer_t *wt = init_wheel_timer(); //initialize wall clock timer
-  start_wheel_timer(wt); /* Wheel timer has started running in a separte thread. */
-
   /* Register the events to be triggered with Wheel timer now. */
-  wheel_timer_elem_t *wt_elem = register_app_event(wt, wrapper_print, "ben_event", strlen("ben_event"),12630, 1);
+  wheel_timer_elem_t *wt_elem = register_app_event(wt, wrapper_print, "ben_event", strlen("ben_event"),12633, 1);
   wt_elem = register_app_event(wt, wrapper_print, "Oana_event", strlen("Oana_event"), 5, 1);  // special case when ben_event and Oana_event is triggered at 15th slot!
+  start_wheel_timer(wt); /* Wheel timer has started running in a separte thread. */
 
   scanf("\n");
   return 0;
